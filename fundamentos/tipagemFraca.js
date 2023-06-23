ca68973bfd2a841b98d354c426577c8a2094be9d
calebe <calebetokyo@gmail.com>
2023-06-23 10:45:50 -0300

    Inserindo mais arquivos

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+// string
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genérico e siglas
+let valor= ''
+let numero = 1
+let pqp = false // Produto Quimico Perigoso... kkk
